a64325e6baea92fa0e884a03328acee2076c9710
MPucheta <smpucheta@gmail.com>
2019-08-15 15:29:07 -0300

    fixed lastname and firstname required message

--- src/authentication/components/util/SignUpSchema/index.js
+++ src/authentication/components/util/SignUpSchema/index.js
@@ -1,8 +1,8 @@
 import * as Yup from 'yup'
 
 const schema = Yup.object().shape({
-  firstname: Yup.string().required('First name is required'),
-  lastname: Yup.string().required('Last name is required'),
+  firstname: Yup.string().required('Firstname is required'),
+  lastname: Yup.string().required('Lastname is required'),
   username: Yup.string().required('Username is required'),
   password: Yup.string().required('Password is required')
 })
